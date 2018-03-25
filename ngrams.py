47b8645f2d4a794d255a0a3b55ab33abb2a32c90
Thirumal Venkat <me@thirumal.in>
2018-03-25 21:05:33 +0530

    Performance enhancements with generators

--- ngrams.py
+++ ngrams.py
@@ -20,61 +20,65 @@ def dialogs(lines):
         for match in regex.finditer(line):
             yield match.groups(0)[0]
 
+PUNCTUATION_TABLE = {ord(c): None for c in set(string.punctuation) - {"'"}}
 
-def nopunct(lines):
+def clean(word):
     """
-    Remove punctuations from a set of lines
-    :param lines: Lines with punctuations
-    :return: Yield a line without punctuation
+    Remove punctuations and whitespaces from a word, finally lowercase
+    the word and yield it.
+
+    :param word: Word with punctuations
+    :return: Yield a word without punctuation
     """
-    table = {ord(c): None for c in set(string.punctuation) - {"'"}}
-    for line in lines:
-        line = line.translate(table)
-        if line:
-            yield line
+    return word.translate(PUNCTUATION_TABLE).strip().lower()
 
 
-def words(lines):
+def words(line):
     """
-    Split the lines into words
-    :param lines: Lines
-    :return: Yield words after stripping punctuations
+    Split the line into words, remove punctuations and whitespaces
+    and finally lowercase the word and yield it
+    :param lines: Line of text
+    :return: Yield words
     """
-    for line in nopunct(lines):
-        for word in line.split():
-            yield word.strip().lower()
+    regex = re.compile(r'(\w+)')
+    for match in regex.finditer(line):
+        yield match.groups(0)[0]
 
 
-def ngrams(lines, num):
+def ngrams(iwords, num):
     """
     Take the lines and yield n-grams from it
-    :param lines: Lines
+    :param iwords: Words iterator
     :param n: Size of each n-gram
     :return: Yield each n-gram one after another
     """
-    w_gen = words(lines)
+    dq_words, iwords = itertools.tee(iwords)
     # Add N - 1 elements to deque, deque has max len of N in n-grams
-    d_q = deque(itertools.islice(w_gen, num - 1), maxlen=num)
-    for word in w_gen:
+    d_q = deque(itertools.islice(dq_words, num - 1), maxlen=num)
+    for word in itertools.islice(iwords, num - 1, None):
         d_q.append(word)
         yield ' '.join(d_q)
 
 
 def parse_and_gen(file_name, ngram_count, item_count):
     """
-    Helper function to read and find the most common words
-    :param file_name: File name having novel
-    :param ngram_count: N-gram size
+    Helper function to read the play, extract dialogs, then
+    remove punctuations and finally split lines into words and
+    geneate n-grams
+    :param file_name: File name having Shakespear's plays in a certain format
+    :param ngram_count: N-gram count for N
     :param item_count: Number of most common items to return
     :return: Yield the counter's top N most common items
     """
     with open(file_name) as lines:
-        counter = Counter(gram for gram in ngrams(dialogs(lines), ngram_count))
+        word_gen = (word for line in dialogs(lines) for word in words(line))
+        counter = Counter(gram for gram in ngrams(word_gen, ngram_count))
         for item in counter.most_common(item_count):
             yield item
 
 
 if __name__ == '__main__':
-    # all.html was taken from http://shakespeare.mit.edu/allswell/full.html
+    # The file "all.html" was taken from
+    # http://shakespeare.mit.edu/allswell/full.html
     for i, ng in enumerate(parse_and_gen('all.html', 4, 10)):
         print(i + 1, ng)
